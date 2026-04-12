dbe0de9ac21768b3d57878bd5044ffb087e75d9c
stav bitanski <stav.bitanski@cyberark.com>
2026-04-12 10:55:53 +0300

    ADB-2720: bump version

--- conanfile.py
+++ conanfile.py
@@ -7,7 +7,7 @@ from conan.tools.scm import Version
 
 class OctoKeygenCPPConan(ConanFile):
     name = "octo-keygen-cpp"
-    version = "1.1.3"
+    version = "1.1.4"
     url = "https://github.com/ofiriluz/octo-keygen-cpp"
     author = "Ofir Iluz"
     settings = "os", "compiler", "build_type", "arch"
